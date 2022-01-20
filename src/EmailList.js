ef50a2460d714235090b99e04294e93dc908ee53
Jumba Mark <jumbamark@yahoo.com>
2022-01-20 09:30:58 +0300

    Rendering the mails from Firestore Database

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,5 +1,5 @@
 import {Checkbox, IconButton} from "@mui/material";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./EmailList.css";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import RedoIcon from "@mui/icons-material/Redo";
@@ -13,8 +13,23 @@ import PeopleIcon from "@mui/icons-material/People";
 import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 import Section from "./Section";
 import EmailRow from "./EmailRow";
+import {db} from "./firebase";
+import {collection, onSnapshot, orderBy} from "firebase/firestore";
 
 function EmailList() {
+  const [emails, setEmails] = useState([]);
+
+  useEffect(() => {
+    onSnapshot(collection(db, "emails"), orderBy("timestamp", "desc"), (snapshot) =>
+      setEmails(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
+
   return (
     <div className="emailList">
       <div className="emailList-settings">
@@ -54,18 +69,21 @@ function EmailList() {
       </div>
 
       <div className="emailList-list">
-        <EmailRow
-          title="Automatives"
-          subject="Exam Announcement"
-          description="This is awesome"
-          time="10pm"
-        />
-        <EmailRow
-          title="Crypto"
-          subject="Cooperate Affairs"
-          description="This is awesome"
-          time="10pm"
-        />
+        {/* {emails.map((email) => ({}))} */}
+
+        {emails.map(({id, data: {to, subject, message, timestamp}}) => {
+          return (
+            <EmailRow
+              id={id}
+              key={id}
+              title={to}
+              subject={subject}
+              description={message}
+              time={new Date(timestamp?.seconds * 1000).toUTCString()}
+            />
+          );
+        })}
+
         <EmailRow
           title="Check this out"
           subject="Hey looser"
@@ -78,3 +96,14 @@ function EmailList() {
 }
 
 export default EmailList;
+
+// I'm targeting the collection of emails in the database and order by the timestamp.
+// onSnapshot is basically a real time listener -any time anything changes over oin the db then it's gonna be updated and this piece of code will fire off again
+// Every time we get a snapshot of the db we setEmails. (the emails columnin firesore - docs)
+// snapshot.docs.map. For every single doc inside of the emails column I wanna return an object (wanna get the id and the data)
+// We set the emails, every time a change happens we update the emails on the frontend and then we map through them and we basically gonna say get me an object with this id and the data is all the stuff on the right as an object
+
+// Go down to the bottom div and rather than hard coded values map through all the emails, for every single email return the following (an EmailRow).
+// Break up the email - when we mapped it we got an id and the data object (break up the data object)
+// When I render out the EmailRow theid and the key will be the id. The key is used so that if we basically have a list of 1000 elements when we push in another element it doesn't have to re render the entire list but instead it just pops in the new one
+// Timestamp would break our code - we use in new Date and put in the timestamp which could be undefined at a given point so you wanna make it optional chaining, you get the seconds multiply by 100 then convert it
