79da9af73f346c78c5b18c3ddf366216948d575f
Jumba Mark <jumbamark@yahoo.com>
2022-01-21 10:41:19 +0300

    Added functionality that persists the login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Route, Routes} from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
@@ -6,28 +6,55 @@ import Sidebar from "./Sidebar";
 import Mail from "./Mail";
 import EmailList from "./EmailList";
 import SendMail from "./SendMail";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {selectSendMessageIsOpen} from "./features/mailSlice";
+import {login, selectUser} from "./features/userSlice";
+import Login from "./Login";
+import {auth} from "./firebase";
+import {onAuthStateChanged} from "firebase/auth";
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // the user is logged in
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        );
+      }
+    });
+  }, []);
 
   return (
-    <div className="app">
-      <Header />
+    <>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app">
+          <Header />
 
-      <div className="app-body">
-        <Sidebar />
+          <div className="app-body">
+            <Sidebar />
 
-        <Routes>
-          <Route path="/" element={<EmailList />} />
-          <Route path="/mail" element={<Mail />} />
-        </Routes>
-      </div>
+            <Routes>
+              <Route path="/" element={<EmailList />} />
+              <Route path="/mail" element={<Mail />} />
+            </Routes>
+          </div>
 
-      {/* If the sendMessageIsOpen the we should render the SendMail */}
-      {sendMessageIsOpen && <SendMail />}
-    </div>
+          {/* If the sendMessageIsOpen the we should render the SendMail */}
+          {sendMessageIsOpen && <SendMail />}
+        </div>
+      )}
+    </>
   );
 }
 
