599f27b47a6776b504eef96d4579382a153f440f
Jumba Mark <jumbamark@yahoo.com>
2022-01-21 10:43:07 +0300

    Added a sign out functionality

--- src/Header.js
+++ src/Header.js
@@ -6,8 +6,28 @@ import SearchIcon from "@mui/icons-material/Search";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import AppsIcon from "@mui/icons-material/Apps";
 import NotificationsIcon from "@mui/icons-material/Notifications";
+import {useDispatch, useSelector} from "react-redux";
+import {logout, selectUser} from "./features/userSlice";
+import {signOut} from "firebase/auth";
+import {auth} from "./firebase";
 
 function Header() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  const SignOut = () => {
+    // We need to logout from firebase
+    signOut(auth)
+      .then((user) => {
+        // Sign-out successful.
+        dispatch(logout());
+      })
+      .catch((error) => {
+        // An error happened.
+        alert(error.message);
+      });
+  };
+
   return (
     <div className="header">
       <div className="header-left">
@@ -34,7 +54,7 @@ function Header() {
           <NotificationsIcon />
         </IconButton>
 
-        <Avatar />
+        <Avatar src={user?.photoUrl} onClick={SignOut} />
       </div>
     </div>
   );
@@ -43,4 +63,5 @@ function Header() {
 export default Header;
 
 // When you fail to wrap it in IconButton you get the icon but it is not a button (there is no ripple effect on clicking)
-//
+
+//Avatar - user? (protect against undefined error)
